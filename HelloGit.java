e5988a63596225c934d4d45a6bf0a1bd33351440
=Aayush Shrivastava <=thebest1@live.in>
2018-07-08 23:37:07 +0530

    Hello Fir

--- HelloGit.java
+++ HelloGit.java
@@ -1,6 +1,7 @@
 public class HelloGit{
     public static void main(String [] args){
     	int x=10;
+    	int y=10;
 	System.out.println("Hello Git!");
 	System.out.println("It's good to know you!");
     }
